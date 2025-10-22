66b6dade447ae8317944dff6259e838d25b64886
anharble <anharble@gmail.com>
2025-10-22 14:39:34 +0700

    Refactor Book class and add new methods

--- Book.cpp
+++ Book.cpp
@@ -1,75 +1,110 @@
 #include "Book.h"
+#include <iomanip>
+ 
+Sach::Sach() {
+    maSach = "";
+    tenSach = "";
+    tacGia = "";
+    nhaXuatBan = "";
+    namXuatBan = 0;
+    soLuong = 0;
+}
 
-Sach::Sach() : namXuatBan(0), soLuong(0), tinhTrang(true) {}
+Sach::~Sach() {}
 
 void Sach::nhapThongTin() {
-    cout << "Nhap ma sach: ";
-    getline(cin, maSach);
     cout << "Nhap ten sach: ";
     getline(cin, tenSach);
     cout << "Nhap tac gia: ";
     getline(cin, tacGia);
     cout << "Nhap nha xuat ban: ";
     getline(cin, nhaXuatBan);
-    cout << "Nhap the loai: ";
-    getline(cin, theLoai);
-    cout << "Nhap ngon ngu: ";
-    getline(cin, ngonNgu);
     cout << "Nhap nam xuat ban: ";
     cin >> namXuatBan;
     cout << "Nhap so luong: ";
     cin >> soLuong;
     cin.ignore();
-    capNhatTinhTrang();
 }
 
 void Sach::hienThiThongTin() const {
-    cout << "\n--- THONG TIN SACH ---\n";
-    cout << "Ma sach: " << maSach << endl;
-    cout << "Ten sach: " << tenSach << endl;
-    cout << "Tac gia: " << tacGia << endl;
-    cout << "Nha xuat ban: " << nhaXuatBan << endl;
-    cout << "The loai: " << theLoai << endl;
-    cout << "Ngon ngu: " << ngonNgu << endl;
-    cout << "Nam xuat ban: " << namXuatBan << endl;
-    cout << "So luong: " << soLuong << endl;
-    cout << "Tinh trang: " << (tinhTrang ? "Con sach" : "Het sach") << endl;
+    cout << "Ma: " << maSach << "| Ten: " << tenSach << "| Tac gia: " << tacGia
+         << "| NXB: " << nhaXuatBan << "| Nam xuat ban: " << namXuatBan
+         << "| So luong: " << soLuong << endl;
 }
 
-void Sach::capNhatTinhTrang() { tinhTrang = (soLuong > 0); }
-
-void Sach::muonSach() {
-    if (soLuong > 0) soLuong--;
-    capNhatTinhTrang();
+const string& Sach::getMaSach() const {
+    return maSach;
+}
+const string& Sach::getTenSach() const {
+    return tenSach;
 }
 
-void Sach::traSach() {
-    soLuong++;
-    capNhatTinhTrang();
+const string& Sach::getTacGia() const {
+    return tacGia;
+}
+const string& Sach::getNhaXuatBan() const {
+    return nhaXuatBan;
+}
+int Sach::getNamXuatBan() const {
+    return namXuatBan;
+}
+int Sach::getSoLuong() const {
+    return soLuong;
 }
 
-void Sach::ghiFile(ofstream &out) const {
-    out << maSach << "," << tenSach << "," << tacGia << ","
-        << nhaXuatBan << "," << theLoai << "," << ngonNgu << ","
-        << namXuatBan << "," << soLuong << "," << tinhTrang << "\n";
+void Sach::setMaSach(const string& m) {
+    maSach = m;
+}
+void Sach::setTenSach(const string& t) {
+    tenSach = t;
+}
+void Sach::setTacGia(const string& tg) {
+    tacGia = tg;
+}
+void Sach::setNhaXuatBan(const string& nxb) {
+    nhaXuatBan = nxb;
+}
+void Sach::setNamXuatBan(int n) {
+    namXuatBan = n;
+}
+void Sach::setSoLuong(int s) {
+    soLuong = s;
 }
 
-bool Sach::docFile(ifstream &in) {
-    string line;
-    if (!getline(in, line)) return false;
-    size_t pos = 0;
-    string data[9];
-    int i = 0;
-    while ((pos = line.find(',')) != string::npos && i < 8) {
-        data[i++] = line.substr(0, pos);
-        line.erase(0, pos + 1);
-    }
-    data[i] = line;
-    if (i < 8) return false;
-
-    maSach = data[0]; tenSach = data[1]; tacGia = data[2];
-    nhaXuatBan = data[3]; theLoai = data[4]; ngonNgu = data[5];
-    namXuatBan = stoi(data[6]); soLuong = stoi(data[7]);
-    tinhTrang = (data[8] == "1" || data[8] == "true");
-    return true;
+void Sach::setautoMaSach(int stt) {
+    // stt: số thứ tự (1-based). Format 4 chữ số, zero-padded.
+    ostringstream oss;
+    oss << prefix();
+    char buf[8];
+    std::sprintf(buf, "%04d", stt);
+    oss << buf;
+    maSach = oss.str();
 }
+
+string Sach::tocsv() const {
+    ostringstream oss;
+   
+    oss << maSach << "," << tenSach << "," << tacGia << "," << nhaXuatBan << "," << namXuatBan << "," << soLuong;
+    return oss.str();
+}
+
+GiaoTrinh::GiaoTrinh() : Sach() {}
+std::string GiaoTrinh::prefix() const { return "GTR"; }
+
+ThamKhao::ThamKhao() : Sach() {}
+std::string ThamKhao::prefix() const { return "STK"; }
+
+TieuThuyet::TieuThuyet() : Sach() {}
+std::string TieuThuyet::prefix() const { return "TIE"; }
+
+TruyenNgan::TruyenNgan() : Sach() {}
+std::string TruyenNgan::prefix() const { return "TRN"; }
+
+TapChi::TapChi() : Sach() {}
+std::string TapChi::prefix() const { return "TAP"; }
+
+TruyenTranh::TruyenTranh() : Sach() {}
+std::string TruyenTranh::prefix() const { return "TRC"; }
+
+SachKiNang::SachKiNang() : Sach() {}
+std::string SachKiNang::prefix() const { return "SKN"; }
