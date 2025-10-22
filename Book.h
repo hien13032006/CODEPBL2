90d04c7699c77d9a31ffea77a7702ee78b2f8bc7
anharble <anharble@gmail.com>
2025-10-22 14:37:34 +0700

    Refactor Sach class and add derived classes

--- Book.h
+++ Book.h
@@ -3,24 +3,81 @@
 
 #include <iostream>
 #include <string>
-#include <fstream>
+
 using namespace std;
 
 class Sach {
-private:
-    string maSach, tenSach, tacGia, nhaXuatBan, theLoai, ngonNgu;
-    int namXuatBan, soLuong;
-    bool tinhTrang;
+    protected:
+        string maSach;
+        string tenSach;
+        string tacGia;
+        string nhaXuatBan;
+        int namXuatBan;
+        int soLuong;
+    public:
+        Sach();
+        virtual ~Sach();
+        virtual void nhapThongTin();
+        virtual void hienThiThongTin() const;
+        const string& getMaSach() const;
+        const string& getTenSach() const;
+        const string& getTacGia() const;
+        const string& getNhaXuatBan() const;
+        int getNamXuatBan() const;
+        int getSoLuong() const;
 
-public:
-    Sach();
-    void nhapThongTin();
-    void hienThiThongTin() const;
-    void capNhatTinhTrang();
-    void muonSach();
-    void traSach();
-    void ghiFile(ofstream &out) const;
-    bool docFile(ifstream &in);
+        void setMaSach(const string& m);
+        void setTenSach(const string& t);
+        void setTacGia(const string& tg);
+        void setNhaXuatBan(const string& nxb);
+        void setNamXuatBan(int n);
+        void setSoLuong(int s);
+
+        virtual string prefix() const = 0;
+
+        void setautoMaSach(int id);
+        string tocsv() const;
 };
 
+class GiaoTrinh : public Sach {
+    public:
+        GiaoTrinh();
+        string prefix() const override;
+};
+
+class ThamKhao : public Sach {
+    public:
+        ThamKhao();
+        string prefix() const override;
+};
+
+class TieuThuyet : public Sach {
+    public:
+        TieuThuyet();
+        string prefix() const override;
+};
+
+class TruyenNgan : public Sach {
+    public:
+        TruyenNgan();
+        string prefix() const override;
+};
+
+class TapChi : public Sach {
+    public:
+        TapChi();
+        string prefix() const override;
+};
+
+class TruyenTranh : public Sach {
+    public:
+        TruyenTranh();
+        string prefix() const override;
+};
+
+class SachKiNang : public Sach {
+    public:
+        SachKiNang();
+        string prefix() const override;
+};
 #endif
