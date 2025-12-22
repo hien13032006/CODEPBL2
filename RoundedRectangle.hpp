bd3dd58e963de6250664b687a1c81b9c39cc86d5
anharble <anharble@gmail.com>
2025-12-22 22:24:55 +0700

    da sua

--- RoundedRectangle.hpp
+++ RoundedRectangle.hpp
@@ -14,7 +14,10 @@ public:
     explicit RoundedRectangleShape(const sf::Vector2f& size = sf::Vector2f(0, 0), float radius = 0, unsigned int cornerPointCount = 10)
         : size(size), radius(radius), cornerPointCount(cornerPointCount) { update(); }
 
-    void setSize(const sf::Vector2f& size) { this->size = size; update(); }
+    void setSize(const sf::Vector2f& size) { 
+        this->size = size;
+        update(); 
+    }
     const sf::Vector2f& getSize() const { return size; }
     void setCornerRadius(float radius) { this->radius = radius; update(); }
     float getCornerRadius() const { return radius; }
@@ -22,17 +25,25 @@ public:
     virtual std::size_t getPointCount() const override { return cornerPointCount * 4; }
 
     virtual sf::Vector2f getPoint(std::size_t index) const override {
-        if (index >= cornerPointCount * 4) return sf::Vector2f(0, 0);
+        if (index >= cornerPointCount * 4) 
+            return sf::Vector2f(0, 0);
         float deltaAngle = 90.0f / (cornerPointCount - 1);
         sf::Vector2f center;
         unsigned int centerIndex = index / cornerPointCount;
         unsigned int pointIndex = index % cornerPointCount;
 
         switch (centerIndex) {
-            case 0: center.x = size.x - radius; center.y = radius; return center + sf::Vector2f(radius * cos((270 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((270 + deltaAngle * pointIndex) * 3.14159 / 180));
-            case 1: center.x = size.x - radius; center.y = size.y - radius; return center + sf::Vector2f(radius * cos((0 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((0 + deltaAngle * pointIndex) * 3.14159 / 180));
-            case 2: center.x = radius; center.y = size.y - radius; return center + sf::Vector2f(radius * cos((90 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((90 + deltaAngle * pointIndex) * 3.14159 / 180));
-            case 3: center.x = radius; center.y = radius; return center + sf::Vector2f(radius * cos((180 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((180 + deltaAngle * pointIndex) * 3.14159 / 180));
+            case 0: center.x = size.x - radius; center.y = radius; 
+                return center + sf::Vector2f(radius * cos((270 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((270 + deltaAngle * pointIndex) * 3.14159 / 180));
+            case 1: center.x = size.x - radius; 
+                center.y = size.y - radius; 
+                return center + sf::Vector2f(radius * cos((0 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((0 + deltaAngle * pointIndex) * 3.14159 / 180));
+            case 2: center.x = radius; 
+                center.y = size.y - radius;
+                return center + sf::Vector2f(radius * cos((90 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((90 + deltaAngle * pointIndex) * 3.14159 / 180));
+            case 3: center.x = radius; 
+                center.y = radius; 
+                return center + sf::Vector2f(radius * cos((180 + deltaAngle * pointIndex) * 3.14159 / 180), radius * sin((180 + deltaAngle * pointIndex) * 3.14159 / 180));
         }
         return sf::Vector2f(0, 0);
     }
