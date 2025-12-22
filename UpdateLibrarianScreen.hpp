742d706ecf0d86f63f894eee66eb8e369fc488ab
anharble <anharble@gmail.com>
2025-12-22 22:06:14 +0700

    da sua

--- UpdateLibrarianScreen.hpp
+++ UpdateLibrarianScreen.hpp
@@ -9,28 +9,97 @@
 
 class UpdateLibrarianScreen {
 private:
-    sf::Font &font; Librarian *librarian; bool closed; RoundedRectangleShape bg; sf::Text title, msg;
-    InputField *inName, *inPhone, *inEmail, *inUser, *inPass; Button *btnSave, *btnCancel;
+    sf::Font &font; 
+    Librarian *librarian; 
+    bool closed; 
+    RoundedRectangleShape bg; 
+    sf::Text title, msg;
+    InputField *inName, *inPhone, *inEmail, *inUser, *inPass; 
+    Button *btnSave, *btnCancel;
 public:
     UpdateLibrarianScreen(sf::Font &f, Librarian *l) : font(f), librarian(l) {
-        closed = false; bg.setSize({600, 650}); bg.setCornerRadius(15.0f); bg.setFillColor(sf::Color::White); bg.setOutlineThickness(2); bg.setOutlineColor(Theme::Primary); bg.setPosition(350, 35);
-        title.setFont(font); title.setString("CAP NHAT THONG TIN"); title.setCharacterSize(32); title.setFillColor(Theme::Primary); title.setPosition(470, 65);
-        float startY = 130; float gap = 70;
-        inName = new InputField({400, startY}, {500, 50}, "Ho ten", font); inName->setText(l->getHoTen());
-        inPhone = new InputField({400, startY+gap}, {500, 50}, "So dien thoai", font); inPhone->setText(l->getSDT());
-        inEmail = new InputField({400, startY+gap*2}, {500, 50}, "Email", font); inEmail->setText(l->getEmail());
-        inUser = new InputField({400, startY+gap*3}, {500, 50}, "Username", font); inUser->setText(l->getUsername());
-        inPass = new InputField({400, startY+gap*4}, {500, 50}, "Password (moi)", font, true); inPass->setText(l->getPassword());
-        btnSave = new Button({400, 580}, {200, 55}, "Luu", font, 0, Theme::Success); btnCancel = new Button({700, 580}, {200, 55}, "Huy", font, 0, sf::Color(150, 150, 150));
-        msg.setFont(font); msg.setCharacterSize(18); msg.setPosition(400, 540);
+        closed = false; 
+        bg.setSize({600, 650}); 
+        bg.setCornerRadius(15.0f); 
+        bg.setFillColor(sf::Color::White); 
+        bg.setOutlineThickness(2); 
+        bg.setOutlineColor(Theme::Primary); 
+        bg.setPosition(350, 35);
+        title.setFont(font); 
+        title.setString("CAP NHAT THONG TIN"); 
+        title.setCharacterSize(32); 
+        title.setFillColor(Theme::Primary); 
+        title.setPosition(470, 65);
+        float startY = 130; 
+        float gap = 70;
+        inName = new InputField({400, startY}, {500, 50}, "Ho ten", font); 
+        inName->setText(l->getHoTen());
+        inPhone = new InputField({400, startY+gap}, {500, 50}, "So dien thoai", font); 
+        inPhone->setText(l->getSDT());
+        inEmail = new InputField({400, startY+gap*2}, {500, 50}, "Email", font); 
+        inEmail->setText(l->getEmail());
+        inUser = new InputField({400, startY+gap*3}, {500, 50}, "Username", font); 
+        inUser->setText(l->getUsername());
+        inPass = new InputField({400, startY+gap*4}, {500, 50}, "Password (moi)", font, true); 
+        inPass->setText(l->getPassword());
+        btnSave = new Button({400, 580}, {200, 55}, "Luu", font, 0, Theme::Success); 
+        btnCancel = new Button({700, 580}, {200, 55}, "Huy", font, 0, sf::Color(150, 150, 150));
+        msg.setFont(font); 
+        msg.setCharacterSize(18); 
+        msg.setPosition(400, 540);
     }
-    ~UpdateLibrarianScreen() { delete inName; delete inPhone; delete inEmail; delete inUser; delete inPass; delete btnSave; delete btnCancel; }
-    bool isClosed() const { return closed; }
+    ~UpdateLibrarianScreen() { 
+        delete inName; 
+        delete inPhone; 
+        delete inEmail; 
+        delete inUser; 
+        delete inPass; 
+        delete btnSave; 
+        delete btnCancel; 
+    }
+    bool isClosed() const { 
+        return closed; 
+    }
     void handleEvent(sf::Event &e, const sf::Vector2f &mouse) {
-        inName->handleEvent(e, mouse); inPhone->handleEvent(e, mouse); inEmail->handleEvent(e, mouse); inUser->handleEvent(e, mouse); inPass->handleEvent(e, mouse);
-        if (e.type == sf::Event::MouseButtonPressed) { if (btnSave->handleClick(mouse)) { librarian->setHoTen(inName->getText()); librarian->setSDT(inPhone->getText()); librarian->setEmail(inEmail->getText()); librarian->setUsername(inUser->getText()); librarian->setPassword(inPass->getText()); msg.setString("Cap nhat thanh cong!"); msg.setFillColor(Theme::Success); closed = true; } if (btnCancel->handleClick(mouse)) closed = true; }
+        inName->handleEvent(e, mouse); 
+        inPhone->handleEvent(e, mouse); 
+        inEmail->handleEvent(e, mouse); 
+        inUser->handleEvent(e, mouse); 
+        inPass->handleEvent(e, mouse);
+        if (e.type == sf::Event::MouseButtonPressed) { 
+            if (btnSave->handleClick(mouse)) { 
+                librarian->setHoTen(inName->getText()); 
+                librarian->setSDT(inPhone->getText()); 
+                librarian->setEmail(inEmail->getText()); 
+                librarian->setUsername(inUser->getText()); 
+                librarian->setPassword(inPass->getText()); 
+                msg.setString("Cap nhat thanh cong!"); 
+                msg.setFillColor(Theme::Success); closed = true; 
+            } 
+            if (btnCancel->handleClick(mouse)) 
+                closed = true; 
+        }
     }
-    void update(sf::Vector2f mouse) { inName->update(); inPhone->update(); inEmail->update(); inUser->update(); inPass->update(); btnSave->update(mouse); btnCancel->update(mouse); }
-    void render(sf::RenderWindow &window) { window.draw(bg); window.draw(title); inName->draw(window); inPhone->draw(window); inEmail->draw(window); inUser->draw(window); inPass->draw(window); btnSave->draw(window); btnCancel->draw(window); window.draw(msg); }
+    void update(sf::Vector2f mouse) { 
+        inName->update(); 
+        inPhone->update(); 
+        inEmail->update(); 
+        inUser->update(); 
+        inPass->update(); 
+        btnSave->update(mouse); 
+        btnCancel->update(mouse); 
+    }
+    void render(sf::RenderWindow &window) { 
+        window.draw(bg); 
+        window.draw(title); 
+        inName->draw(window); 
+        inPhone->draw(window); 
+        inEmail->draw(window); 
+        inUser->draw(window); 
+        inPass->draw(window); 
+        btnSave->draw(window); 
+        btnCancel->draw(window); 
+        window.draw(msg); 
+    }
 };
 #endif
